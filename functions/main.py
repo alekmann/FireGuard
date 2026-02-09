5de3f638d7e36838d68d6213a2ba2dac752a91ac
alekmann <aleroe99@gmail.com>
2026-02-09 10:11:56 +0100

    minimal deployment

--- functions/main.py
+++ functions/main.py
@@ -1,21 +1,5 @@
-from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from dotenv import load_dotenv
-from app.firebase import init_firebase
-from app.api import router as api_router
+from firebase_functions import https_fn
 
-load_dotenv()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    init_firebase()
-    yield
-
-
-app = FastAPI(
-    title="FireGuard API",
-    lifespan=lifespan,
-)
-
-app.include_router(api_router)
+@https_fn.on_request()
+def hello(req):
+    return "Firebase Python deploy works!"
